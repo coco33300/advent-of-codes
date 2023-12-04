89d9b70ed86278feaec18838d04d4bed9efd2b4c
cdavidenko <corentin.davidenko@up.coop>
2023-12-04 16:54:56 +0100

    wip day3, wrong answer 528256 on input.txt

--- helpers/file.utils.ts
+++ helpers/file.utils.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 
 export function getLinesFromFile(fileName: string) {
-    return fs.readFileSync(fileName, {encoding: "utf-8"}).split('\n');
+    return fs.readFileSync(fileName, {encoding: "utf-8"}).split('\r\n');
 }
