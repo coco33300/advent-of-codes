7794c65221b1e8133f8534f210dc8c6a5e016c67
Corentin Davidenko <corentin.davidenko@up.coop>
2023-12-13 19:15:24 +0100

    day 7 mal factorisé entre les parts (#8)
    Ne sera pas refacto

--- helpers/file.utils.ts
+++ helpers/file.utils.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 export function getLinesFromFile(fileName: string) {
     try {
-        return fs.readFileSync(fileName, {encoding: "utf-8"}).split('\r\n');
+        return fs.readFileSync(fileName, {encoding: "utf-8"}).trim().split('\r\n');
     } catch (e) {
         return [];
     }
