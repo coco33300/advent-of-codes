774c61c8a1ad33c0186b4de436decb798cf2cd91
Corentin Davidenko <corentin.davidenko@up.coop>
2023-12-14 23:59:45 +0100

    day 10 wip too much nodes find the loop

--- day10/part1.ts
+++ day10/part1.ts
@@ -1,3 +1,85 @@
 import { getLinesFromFile } from '../helpers/file.utils';
+import { pipe, S, J, L, dot, dash, F, seven, symbolsList, PipeNode, Symbols } from './type';
 
-const lines = getLinesFromFile('input.txt');
+const lines = getLinesFromFile('input.txt').map(l => l.trim());
+
+const startLine = lines.find(l => l.search(/S/) > -1);
+const lineIndex = lines.indexOf(startLine);
+
+const nodes: PipeNode[][] = Array.from({length: lines.length}, () => Array(lines[0].length));
+
+const pipeRoot: PipeNode = {
+    x: startLine.search(/S/),
+    y: lineIndex,
+    symbol: startLine[lineIndex] as Symbols,
+    distance: 0,
+}
+
+nodes[pipeRoot.y][pipeRoot.y] = pipeRoot;
+
+const stack = [pipeRoot];
+const knownNodes = [pipeRoot];
+
+// Parcours en largeur depuis le départ pour reconstruire le tuyau
+while (stack.length > 0) {
+    const node = stack.pop();
+    const {x, y, symbol, distance} = node
+    const newNode = (xNew, yNew): PipeNode => ({
+        symbol: lines[yNew][xNew] as Symbols,
+        y: yNew,
+        x: xNew,
+        distance: distance + 1
+    })
+
+    // North
+    if ((<string[]>[pipe, L, J, S]).includes(symbol) && y > 0) {
+        if (!nodes[y - 1][x] && lines[y -1][x] !== dot) {
+            const node: PipeNode = newNode(x, y -1);
+            nodes[y - 1][x] = node;
+            knownNodes.push(node)
+            stack.push(node)
+        } else if (nodes[y - 1][x]?.distance && nodes[y - 1][x]?.distance > distance + 1) {
+            nodes[y - 1][x].distance = distance + 1;
+        }
+    }
+
+    // South
+    if ((<string[]>[pipe, seven, F, S]).includes(symbol) && y < lines.length -1) {
+        if (!nodes[y + 1][x] && lines[y +1][x] !== dot) {
+            const node: PipeNode = newNode(x, y +1);
+            nodes[y + 1][x] = node;
+            knownNodes.push(node)
+            stack.push(node)
+        } else if (nodes[y + 1][x]?.distance && nodes[y + 1][x]?.distance > distance + 1) {
+            nodes[y + 1][x].distance = distance + 1;
+        }
+    }
+
+    // West
+    if ((<string[]>[dash, J, seven, S]).includes(symbol) && x > 0) {
+        if (!nodes[y][x - 1] && lines[y][x - 1] !== dot) {
+            const node: PipeNode = newNode(x - 1, y);
+            nodes[y][x - 1] = node;
+            knownNodes.push(node)
+            stack.push(node)
+        } else if (nodes[y][x - 1]?.distance && nodes[y][x - 1]?.distance > distance + 1) {
+            nodes[y][x - 1].distance = distance + 1;
+        }
+    }
+    // East
+    if ((<string[]>[dash, L, F, S]).includes(symbol) && x < lines[0].length -1) {
+        if (!nodes[y][x + 1] && lines[y][x + 1] !== dot) {
+            const node: PipeNode = newNode(x + 1, y);
+            nodes[y][x + 1] = node;
+            knownNodes.push(node)
+            stack.push(node)
+        } else if (nodes[y][x + 1]?.distance && nodes[y][x + 1]?.distance > distance + 1) {
+            nodes[y][x + 1].distance = distance + 1;
+        }
+    }
+}
+
+console.log(nodes)
+
+// cull Nodes with less than 2 neighbours based on their symbol.?
+// Go from root
---
+++ day10/type.ts
@@ -0,0 +1,43 @@
+//    | is a vertical pipe connecting north and south.
+export const pipe = '|' as const;
+
+//    - is a horizontal pipe connecting east and west.
+export const dash = '-' as const;
+
+//    L is a 90-degree bend connecting north and east.
+export const L = 'L' as const;
+
+//    J is a 90-degree bend connecting north and west.
+export const J = 'J' as const;
+
+//    7 is a 90-degree bend connecting south and west.
+export const seven = '7' as const;
+
+//    F is a 90-degree bend connecting south and east.
+export const F = 'F' as const;
+
+//    . is ground; there is no pipe in this tile.
+export const dot = '.' as const;
+
+//    S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+export const S = 'S' as const;
+
+export const symbolsList = [
+    pipe,
+    dash,
+    L,
+    J,
+    seven,
+    F,
+    dot,
+    S,
+]
+
+export type Symbols = typeof symbolsList[number];
+
+export type PipeNode = {
+    x: number,
+    y: number,
+    symbol: Symbols,
+    distance: number,
+}
