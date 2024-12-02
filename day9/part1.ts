3c8b4753ec9e62473f85e2f1cf9c5f445998e21a
Corentin Davidenko <corentin.davidenko@up.coop>
2024-12-02 10:12:10 +0100

    day9 (#12)

---
+++ day9/part1.ts
@@ -0,0 +1,24 @@
+import { getLinesFromFile } from '../helpers/file.utils';
+
+const lines = getLinesFromFile('input.txt');
+
+const histories = lines.map(l => l.match(/-?\d+/g).map(m => +m));
+
+const getExtrapolation = (sequence: number[]) => {
+    if (sequence.every(s => s === 0)) {
+        return 0;
+    }
+
+    const nextSequence = [];
+    for (let i = 1; i < sequence.length; i++) {
+        nextSequence.push(sequence[i] - sequence[i - 1]);
+    }
+    return sequence[sequence.length - 1] + getExtrapolation(nextSequence)
+}
+
+const extrapolatedValues = histories.map(h => getExtrapolation(h))
+const extrapolatedValues2 = histories.map(h => getExtrapolation(h.reverse()))
+
+console.log('part1', extrapolatedValues.reduce((acc, e) => acc + e))
+console.log('part2', extrapolatedValues2.reduce((acc, e) => acc + e))
+
