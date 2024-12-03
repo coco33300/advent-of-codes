85540b51615632d888c80db08ff4df005016f261
cdavidenko <corentin.davidenko@up.coop>
2024-12-03 11:21:32 +0100

    day3 part2

--- 2024/day3/day3-2.ts
+++ 2024/day3/day3-2.ts
@@ -3,11 +3,37 @@ import { getLinesFromFile } from "../../helpers/file.utils";
 const lines = getLinesFromFile('input.txt').join();
 
 // const regex = RegExp(/mul\((?:\d+),(?:\d+)\)/m);
-const regex = RegExp(/mul\(\d+,\d+\)/gm);
+// const regex = RegExp(/mul\(\d+,\d+\)/gm);
 
-const muls =  lines.match(regex)
+// only muls after do works
+// const dos = /do\(\)/gm
+// const donts = /don\'t\(\)/gm;
+
+const regex = RegExp(/(mul\(\d+,\d+\))|(do\(\))|(don\'t\(\))/gm);
+
+const matches = lines.match(regex) || [];
+
+let take = true;
+const muls = []
+// console.log(matches)
+for (const match of matches) {
+
+    if (match.startsWith ('do(')) {
+        take = true;
+        continue;
+    }
+    if (match.startsWith ('don')) {
+        take = false;
+        continue;
+    }
+    if (take) {
+        muls.push(match)
+    }
+}
+
+
 const operands = muls.map(s => s.match(/\d+/g))
 const multipy = operands.map(([o1, o2]) => +o1 * +o2)
 const sum = multipy.reduce((sum, o) => sum + o);
 
-console.log(operands, multipy, sum)
+console.log(sum)
